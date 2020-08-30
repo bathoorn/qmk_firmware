318d2015f6b7180f40b01247bf29600bf17f5419
Ronnie Bathoorn <b@thoorn.com>
2020-08-30 16:15:44 +0200

    add extra layers

--- keyboards/qaz/keymaps/ronnie2/keymap.c
+++ keyboards/qaz/keymaps/ronnie2/keymap.c
@@ -2,56 +2,86 @@
 
 enum layers{
   _BASE,
-  _NUM_SYM,
-  _NAV
+  _LOWER,
+  _RAISE,
+  _ADJUST
 };
 enum combo_events {
-  COMBO_BSPC,
-  COMBO_NUMBAK,
   COMBO_TAB,
   COMBO_ESC,
-  COMBO_DEL,
+  COMBO_QUOTE,
+  COMBO_BSLS,
 };
 
-#define KC_NUM_SPC LT(_NUM_SYM, KC_SPC)
-#define KC_GA LGUI_T(KC_A)
-#define KC_AS LALT_T(KC_S)
-#define KC_CD LCTL_T(KC_D)
-#define KC_SF LSFT_T(KC_F)
-#define KC_SJ RSFT_T(KC_J)
-#define KC_CK RCTL_T(KC_K)
-#define KC_AL RALT_T(KC_L)
-#define KC_GSCLN RGUI_T(KC_SCLN)
+// layer switching
+
+#define LO(K01)  LT(_LOWER , K01)
+#define RS(K01)  LT(_RAISE , K01)
+#define AD(K01)  LT(_ADJUST, K01)
+
+// modifier keys
+
+#define LC(K01) LCTL_T(K01)
+#define LA(K01) LALT_T(K01)
+#define LG(K01) LGUI_T(K01)
+#define LS(K01) LSFT_T(K01)
+#define RC(K01) RCTL_T(K01)
+#define RA(K01) LALT_T(K01)
+#define RG(K01) LGUI_T(K01)
+#define RS(K01) LSFT_T(K01)
+
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_split_space(
-    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,    KC_I,   KC_O,     KC_P,
-    KC_GA,  KC_AS,  KC_CD,  KC_SF,   KC_G,   KC_H,  KC_SJ,   KC_CK,  KC_AL, KC_ENT,
+	KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
+	LC(KC_A), LG(KC_S), LA(KC_D), LS(KC_F), KC_G, KC_H, RS(KC_J), RA(KC_K), RG(KC_L), RC(KC_SCLN),
     KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M, KC_COMM, KC_DOT,
-    KC_LCTL, KC_LALT, KC_LGUI, KC_BSPC, LT(_NUM_SYM,KC_SPACE), KC_RALT, KC_RGUI, KC_RCTL
+    KC_LCTL, KC_LGUI, RS(KC_DEL), KC_SPACE, KC_ENT, LO(KC_BKSP), KC_RGUI, KC_RCTL
   ),
 
-  [_NUM_SYM] = LAYOUT_split_space(
-    KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
-    KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_EQUAL, KC_MINS,
-    KC_LCBR, KC_LBRC, KC_LPRN, KC_UNDS, KC_RPRN, KC_RBRC, KC_RCBR, KC_SCLN, KC_QUOTE,
-    RGB_TOG, RGB_MOD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [_LOWER] = LAYOUT_split_space(
+    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6  , KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,
+    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12 , KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,
+    KC_LCTL, KC_LGUI, RS(KC_DEL), KC_SPACE, KC_ENT, LO(KC_BKSP), KC_RGUI, KC_RCTL
+  ),
+
+  [_RAISE] = LAYOUT_split_space(
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+    KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_LCTL, KC_LGUI, RS(KC_DEL), KC_SPACE, KC_ENT, LO(KC_BKSP), KC_RGUI, KC_RCTL
+  ),
+
+  [_ADJUST] = LAYOUT_split_space(
+    RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_TOG,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    MU_TOG , CK_TOGG, AU_ON,   AU_OFF,  AG_NORM,  AG_SWAP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    RGB_RMOD,RGB_HUD,RGB_SAD, RGB_VAD, KC_RGB_T,  MG_NKRO, KC_MUTE, KC_VOLD, KC_VOLU, KC_MNXT,
+    KC_LCTL, KC_LGUI, RS(KC_DEL), KC_SPACE, KC_ENT, LO(KC_BKSP), KC_RGUI, KC_RCTL
   )
 };
 
 #ifdef COMBO_ENABLE
-const uint16_t PROGMEM combo_bspc[] = {KC_O, KC_P, COMBO_END};
-const uint16_t PROGMEM combo_numbak[] = {KC_0, KC_9, COMBO_END};
+const uint16_t PROGMEM combo_bsls[] = {KC_O, KC_P, COMBO_END};
+const uint16_t PROGMEM combo_quote[] = {KC_L, KC_SCLN, COMBO_END};
 const uint16_t PROGMEM combo_tab[] = {KC_Q, KC_W, COMBO_END};
-const uint16_t PROGMEM combo_esc[] = {KC_E, KC_W, COMBO_END};
-const uint16_t PROGMEM combo_del[] = {KC_MINS, KC_EQL, COMBO_END};
+const uint16_t PROGMEM combo_esc[] = {KC_A, KC_S, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
-  [COMBO_BSPC] = COMBO(combo_bspc,KC_BSPC),
-  [COMBO_NUMBAK] = COMBO(combo_numbak,KC_BSPC),
+  [COMBO_BSLS] = COMBO(combo_bsls,KC_BSLS),
+  [COMBO_QUOTE] = COMBO(combo_quote,KC_QUOTE),
   [COMBO_TAB] = COMBO(combo_tab,KC_TAB),
   [COMBO_ESC] = COMBO(combo_esc,KC_ESC),
-  [COMBO_DEL] = COMBO(combo_del,KC_DEL),
 
 };
 #endif
+
+layer_state_t layer_state_set_keymap(layer_state_t state);
+
+// on layer change, no matter where the change was initiated
+// Then runs keymap's layer change check
+layer_state_t layer_state_set_user(layer_state_t state) {
+    state = update_tri_layer_state(state, _RAISE, _LOWER, _ADJUST);
+    return layer_state_set_keymap(state);
+}
