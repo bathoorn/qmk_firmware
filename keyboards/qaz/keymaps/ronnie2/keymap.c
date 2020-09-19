645ff0451116f327c44ac4b5d2b7bfed74c2c781
Ronnie Bathoorn <b@thoorn.com>
2020-09-19 13:10:39 +0200

    little fixes to add keys to vim layer

--- keyboards/qaz/keymaps/ronnie2/keymap.c
+++ keyboards/qaz/keymaps/ronnie2/keymap.c
@@ -44,8 +44,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_VIM] = LAYOUT_split_space(
 	KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
 	KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN,
-    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M, KC_DLR, KC_LSFT,
-    KC_0,    KC_1,    KC_2, KC_SPACE, KC_ENT, KC_3,    KC_4,    KC_5
+    KC_RSFT, KC_X, KC_C, KC_V, KC_B, KC_COMM, KC_DOT, KC_DLR, KC_LSFT,
+    KC_0, KC_1, KC_2, KC_SPACE, KC_ENT, KC_3, KC_4, KC_5
   )
 };
 
